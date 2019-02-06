911976f663ff35b76d2b6e9d3d61bf3b215112fd
Igor Rumiha <igor.rumiha@oradian.com>
2019-02-06 13:24:48 +0100

    Split out login and signup request.

---
+++ src/main/scala/org/codecannery/lunchplanner/domain/authentication/command/SignupRequest.scala
@@ -0,0 +1,21 @@
+package org.codecannery.lunchplanner.domain.authentication.command
+import org.codecannery.lunchplanner.domain.users.command.CreateUser
+import tsec.passwordhashers.PasswordHash
+
+final case class SignupRequest(
+    userName: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    phone: String,
+) {
+  def asCreateUser[A](hashedPassword: PasswordHash[A]): CreateUser = CreateUser(
+    userName,
+    firstName,
+    lastName,
+    email,
+    hashedPassword.toString,
+    phone
+  )
+}
