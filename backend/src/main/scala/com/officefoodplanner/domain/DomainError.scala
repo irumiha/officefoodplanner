3f10c708f96525a53ec01f342697beeb2d43524d
Igor Rumiha <igorrumiha@gmail.com>
2019-09-13 00:49:20 +0200

    ErrorT cleanup.

--- backend/src/main/scala/com/officefoodplanner/domain/DomainError.scala
+++ backend/src/main/scala/com/officefoodplanner/domain/DomainError.scala
@@ -1,3 +1,35 @@
 package com.officefoodplanner.domain
 
+import cats.{Applicative, Functor}
+import cats.data.EitherT
+
+
 trait DomainError extends Exception with Product with Serializable
+
+trait ErrorT[E <: DomainError] {
+
+  implicit class feitherToEitherT[F[_], A](r: F[Either[E,A]]) {
+    def wrapT: EitherT[F, E, A] = EitherT(r)
+  }
+  implicit class foptionToEitherT[F[_]: Functor, A](o: F[Option[A]]) {
+    // Make a LeftF containing the error E if the option is empty
+    def orError(e: E): EitherT[F, E, A] = EitherT.fromOptionF(o, e)
+
+    // If the option is NOT empty make a LeftF with the contents of the option
+    def liftError: EitherT[F, A, Unit] = EitherT(Functor[F].map(o)(_.fold[Either[A, Unit]](Right(()))(Left(_))))
+
+    // Make a LeftF containing the error E if the option is FULL (inverse of orError)
+    def errorIfFull(e: E): EitherT[F, E, Unit] = EitherT(
+      Functor[F].map(o) {
+        case None => Right(())
+        case Some(_) => Left(e)
+      }
+    )
+    def errorIfFound(e: E): EitherT[F, E, Unit] = errorIfFull(e)
+  }
+
+  implicit class fanyToRightT[F[_]: Applicative, A](a: F[A]) {
+    def rightF: EitherT[F, E, A] = EitherT.right(a)
+  }
+
+}
--- backend/src/main/scala/com/officefoodplanner/domain/auth/UserService.scala
+++ backend/src/main/scala/com/officefoodplanner/domain/auth/UserService.scala
@@ -8,45 +8,17 @@ import cats.arrow.FunctionK
 import cats.data.EitherT
 import cats.effect.Async
 import cats.implicits._
-
 import io.scalaland.chimney.dsl._
 import tsec.common.Verified
 import tsec.passwordhashers.{PasswordHash, PasswordHasher}
+
 import com.officefoodplanner.config.ApplicationConfig
-import com.officefoodplanner.domain.DomainError
+import com.officefoodplanner.domain.ErrorT
 import com.officefoodplanner.domain.auth.command.{CreateUser, UpdateUser, UpdateUserPassword}
 import com.officefoodplanner.domain.auth.model.User
 import com.officefoodplanner.domain.auth.repository.UserRepository
 import com.officefoodplanner.domain.auth.view.UserSimpleView
 import com.officefoodplanner.infrastructure.service.TransactingService
-
-trait ErrorT[E <: DomainError] {
-
-  implicit class feitherToEitherT[F[_], A](r: F[Either[E,A]]) {
-    def wrapT: EitherT[F, E, A] = EitherT(r)
-  }
-  implicit class foptionToEitherT[F[_]: Functor, A](o: F[Option[A]]) {
-    // Make a LeftF containing the error E if the option is empty
-    def orError(e: E): EitherT[F, E, A] = EitherT.fromOptionF(o, e)
-
-    // If the option is NOT empty make a LeftF with the contents of the option
-    def liftError: EitherT[F, A, Unit] = EitherT(Functor[F].map(o)(_.fold[Either[A, Unit]](Right(()))(Left(_))))
-
-    // Make a LeftF containing the error E if the option is FULL (inverse of orError)
-    def errorIfFull(e: E): EitherT[F, E, Unit] = EitherT(
-      Functor[F].map(o) {
-        case None => Right(())
-        case Some(_) => Left(e)
-      }
-    )
-    def errorIfFound(e: E): EitherT[F, E, Unit] = errorIfFull(e)
-  }
-
-  implicit class fanyToRightT[F[_]: Applicative, A](a: F[A]) {
-    def rightF: EitherT[F, E, A] = EitherT.right(a)
-  }
-
-}
 
 abstract class UserService[F[_]: Monad, D[_]: Async, H]
   extends TransactingService[F, D]
